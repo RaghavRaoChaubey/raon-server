20617af9a7a089fd030182bc726317b044319db5
RaghavRaoChaubey <raghavraochaubey@gmail.com>
2026-02-14 17:12:05 +0530

    fix render port

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-import "./server.js";
-
 import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("RÆON backend is alive 😈");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-app.get("/", (req, res) => {
-  res.send("RÆON SERVER ONLINE");
+  res.send("RÆON SERVER ONLINE 😈");
 });
 
 app.get("/ping", (req, res) => {
-  res.send("RÆON SERVER ONLINE");
+  res.send("RÆON SERVER ONLINE 😈");
 });
 
+// IMPORTANT PART
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
