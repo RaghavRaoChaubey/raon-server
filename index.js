b3a4012439df4bd76d83762b8a72029a0cb34002
RaghavRaoChaubey <raghavraochaubey@gmail.com>
2026-02-14 17:24:42 +0530

    add license api

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const validKeys = [
+  "RAON-DEV-9999",
+  "RAON-ALPHA-1234"
+];
 
 app.get("/", (req, res) => {
   res.send("RÆON SERVER ONLINE 😈");
 });
 
-app.get("/ping", (req, res) => {
-  res.send("RÆON SERVER ONLINE 😈");
+app.post("/license/verify", (req, res) => {
+  const { key } = req.body;
+
+  if (validKeys.includes(key)) {
+    res.json({ status: "ok", plan: "hacker" });
+  } else {
+    res.json({ status: "invalid" });
+  }
 });
 
-// IMPORTANT PART
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on " + PORT);
 });
